9ccdddfcfbaf14de08044c7b7922781e82b7b594
Crackminer <schrewrepatrick@gmail.com>
2024-01-24 09:53:18 +0100

    I hope only database calls and acknowledgement windows missing

--- src/app/employee-editor/employee-editor.component.ts
+++ src/app/employee-editor/employee-editor.component.ts
@@ -10,6 +10,7 @@ import {EmployeeListComponent} from "../employee-list/employee-list.component";
 export class EmployeeEditorComponent {
 
   @Input() employee?: Employee;
+  ogEmployee: Employee = new Employee();
   isDisabled = true;
 
   constructor(private listComponent: EmployeeListComponent) {
@@ -23,10 +24,11 @@ export class EmployeeEditorComponent {
     this.listComponent.resetSelectedEmployee();
   }
 
-  edit() {
+  edit(employee: Employee) {
     //make buttons editable
     //make other buttons visible and hide some buttons
     this.isDisabled = false;
+    this.ogEmployee = employee;
   }
 
   delete() {
@@ -37,5 +39,39 @@ export class EmployeeEditorComponent {
     this.employee = undefined;
     this.isDisabled = true;
     this.listComponent.resetSelectedEmployee();
+  }
+
+  save() {
+    //call database to add new employee or to update existing employee
+    if (this.employee !== undefined)
+    {
+      if (this.ogEmployee.id === new Employee().id)
+      {
+        //add new employee
+      }
+      else
+      {
+        //update existing employee
+      }
+      //run acknowledgement window
+
+      //on successful acknowledge go back to  detail view if edited and go back to all view if added
+      if (this.ogEmployee.id === new Employee().id)
+      {
+        //added new employee
+        this.back();
+      }
+      else
+      {
+        //updated existing employee
+        this.ogEmployee = this.employee;
+        this.isDisabled = true;
+      }
+    }
+  }
+
+  undo() {
+    //get rid of user input
+    this.employee = this.ogEmployee;
   }
 }
